6e1bb12ac0cd0c1d1c729d38311f6fc1c5c1d963
Hu Kaixiang <maxhu787@gmail.com>
2024-02-28 19:16:16 +0800

    e

---
+++ app.js
@@ -0,0 +1,24 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if(entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((elem) => observer.observe(elem))
+
+const observerCert = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('cert-show');
+    } else {
+      entry.target.classList.remove('cert-show');
+    }
+  })
+})
